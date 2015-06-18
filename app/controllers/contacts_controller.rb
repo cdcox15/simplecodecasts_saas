6322297422efbde51666b2e66e1d2c640afaa093
Daniel Cox <coxdaniel15@gmail.com>
2015-06-18 14:55:13 -0700

    updated email details

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
 	end
 
 	def create 
-		@contact = Contact.new(contact_params)	
+		@contact = Contact.new(contact_params)	 
 		if @contact.save
 			name = params[:contact][:name]
 			email = params[:contact][:email]
