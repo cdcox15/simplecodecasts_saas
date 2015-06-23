e5080ec186ed771768b7cf43e346a63dcf1172b0
Daniel Cox <coxdaniel15@gmail.com>
2015-06-23 11:12:13 -0700

    updated sign out link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,7 +15,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	end
 
 	private
-	def seclect_plan
+	def select_plan
 		unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
 			flash[:notice] = 'Please select a membership plan to sign up.'
 			redirect_to root_url
