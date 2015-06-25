74b9f0533224f4dd89cebca28144a1b15e254e3f
Daniel Cox <coxdaniel15@gmail.com>
2015-06-25 15:28:25 -0700

    home page improvements

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,14 +3,14 @@ class ProfilesController < ApplicationController
 	def new
 		# form where a user can fill out their own profile
 		@user = User.find( params[:user_id] )
-		@profile = @user.build_profile
+		@profile = Profile.new
 	end
 
 	def create
 		@user = User.find( params[:user_id] )
 		@profile = @user.build_profile(profile_params)
 		if @profile.save
-			flash[:success] = "Profile Updated!"
+			flash[:success] = "Profile Updated!" 
 			redirect_to user_path( params[:user_id] )
 		else
 			render action :new
